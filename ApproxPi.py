ce4ae5f71868b6f9660a331e99944373147e4d12
treyruge15 <treyruge15@gmail.com>
2025-09-14 17:30:18 +0000

    ApproxPi -- Trey Ruge, Lab 3

--- ApproxPi.py
+++ ApproxPi.py
@@ -12,9 +12,19 @@ def main():
   realPi = math.pi
 
   #ask user for decimal percision (up to 10)
-
+  num1 = input("Enter how many decimal places you would like to round Pi to (Limit of 10): ")
+  num2 = int(num1)
   start = time.time()
   #calculate pi using the approximation technique
+  approxPi = 4
+  sign = -1
+  denominator = 3
+  while round(approxPi,num2) != round(realPi,num2):
+    print(approxPi)
+    approxPi = approxPi + (sign * 4 / denominator)
+
+    sign = sign * -1
+    denominator = denominator + 2
   #Loop until the level of percision is reached
 
   end = time.time()
